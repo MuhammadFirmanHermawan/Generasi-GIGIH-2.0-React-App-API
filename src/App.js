6bb17d48507681f265c7aaa18f5be048b23c0739
MuhammadFirmanHermawan <muhammadfirmanhermawan0607@gmail.com>
2022-03-28 09:57:19 +0700

    Maps and Loops in JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from "react";
 import "./App.css";
 import Music from "./components/detail/index";
-import data from "./components/data.js";
+import data from "./components/data";
 
 function App() {
   return (
     <div className="App-header">
       <div className="music">
-        <Music
-          image={<img className="music-img" src={data.album.images[0].url} />}
-          artist={<h1 id="music">{data.artists[0].name}</h1>}
-          title={<h1 id="music">{data.album.name}</h1>}
-          album={<h1 id="music">{data.name}</h1>}
-          select={
-            <button id="button" onClick={data.album.external_urls.spotify}>
-              Select
-            </button>
-          }
-        />
+        {data.map((datas) => (
+          <div key={datas.id}>
+            <Music image={datas.album.images[0].url} title={datas.album.name} artist={datas.artists[0].name} album={datas.name} />
+            <button id="button">Select</button>
+          </div>
+        ))}
       </div>
     </div>
   );
 }
 export default App;
+// ini adalah component
+// function App() {
+//   return (
+//     <div className="App-header">
+//       <div className="music">
+//         <Music
+//           image={<img className="music-img" src={data.album.images[0].url} />}
+//           artist={<h1 id="music">{data.artists[0].name}</h1>}
+//           title={<h1 id="music">{data.album.name}</h1>}
+//           album={<h1 id="music">{data.name}</h1>}
+//           select={
+//             <button id="button" onClick={data.album.external_urls.spotify}>
+//               Select
+//             </button>
+//           }
+//         />
+//       </div>
+//     </div>
+//   );
+// }
+// export default App;
--- src/components/detail/index.js
+++ src/components/detail/index.js
@@ -1,12 +1,28 @@
-function Music(props) {
+import React from "react";
+
+const Music = (props) => {
+  const { image, title, artist, album } = props;
   return (
-    <div className="music">
-      <div>{props.image}</div>
-      <div>{props.title}</div>
-      <div>{props.artist}</div>
-      <div>{props.album}</div>
-      <div>{props.select}</div>
+    <div>
+      <img className="music-img" src={image} />
+      <h2 id="music">{title}</h2>
+      <h2 id="music">{artist}</h2>
+      <h2 id="music">{album}</h2>
     </div>
   );
-}
+};
+
 export default Music;
+// ini adalah component
+// function Music(props) {
+//   return (
+//     <div className="music">
+//       <div>{props.image}</div>
+//       <div>{props.title}</div>
+//       <div>{props.artist}</div>
+//       <div>{props.album}</div>
+//       <div>{props.select}</div>
+//     </div>
+//   );
+// }
+// export default Music;
