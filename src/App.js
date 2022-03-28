5334757b057481e3e6553aae17a6a65dc899e1e9
MuhammadFirmanHermawan <muhammadfirmanhermawan0607@gmail.com>
2022-03-28 10:02:44 +0700

    Maps and Loops in JSX

--- src/App.js
+++ src/App.js
@@ -18,24 +18,3 @@ function App() {
   );
 }
 export default App;
-// ini adalah component
-// function App() {
-//   return (
-//     <div className="App-header">
-//       <div className="music">
-//         <Music
-//           image={<img className="music-img" src={data.album.images[0].url} />}
-//           artist={<h1 id="music">{data.artists[0].name}</h1>}
-//           title={<h1 id="music">{data.album.name}</h1>}
-//           album={<h1 id="music">{data.name}</h1>}
-//           select={
-//             <button id="button" onClick={data.album.external_urls.spotify}>
-//               Select
-//             </button>
-//           }
-//         />
-//       </div>
-//     </div>
-//   );
-// }
-// export default App;
--- src/components/detail/index.js
+++ src/components/detail/index.js
@@ -13,16 +13,3 @@ const Music = (props) => {
 };
 
 export default Music;
-// ini adalah component
-// function Music(props) {
-//   return (
-//     <div className="music">
-//       <div>{props.image}</div>
-//       <div>{props.title}</div>
-//       <div>{props.artist}</div>
-//       <div>{props.album}</div>
-//       <div>{props.select}</div>
-//     </div>
-//   );
-// }
-// export default Music;
