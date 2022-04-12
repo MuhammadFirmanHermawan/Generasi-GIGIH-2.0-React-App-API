5ce31edeb7874d0f7e19a62a71d2007156e33b6d
MuhammadFirmanHermawan <muhammadfirmanhermawan0607@gmail.com>
2022-04-12 21:35:57 +0700

    Flexbox

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import "./components/spotify-app/style.css";
 import Spotify from "./components/spotify-app/index";
-import Routing from "./components/spotify-app/route";
 
 function App() {
   return <Spotify />;
